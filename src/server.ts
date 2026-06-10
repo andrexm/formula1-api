2179577d5f0afc8c105df37f7cae6fc462785ba3
André Nascimento <andre.neospace@gmail.com>
2026-06-10 18:21:57 -0300

    feat: basic routes

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,53 @@
-console.log("hello world");
+import fastify from "fastify"
+import cors from "@fastify/cors"
+
+const server = fastify({ logger: true })
+
+server.register(cors, {
+  origin: "*"
+})
+
+const teams = [
+  { id: 1, name: "Ferrari", base: "" },
+  { id: 2, name: "McLaren", base: "Woking, United Kingdom" },
+  { id: 3, name: "Mercedes", base: "Brackley, United Kingdom" },
+  { id: 4, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+]
+
+const drivers = [
+  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
+  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
+  { id: 3, name: "Nando Morris", team: "McLaren" },
+  //{ id: , name: "", team: "" },
+]
+
+server.get("/teams", async (request, response) => {
+  response.type("application/json").code(200)
+  return { teams }
+})
+
+server.get("/drivers", async (request, response) => {
+  response.type("application/json").code(200)
+  return drivers
+})
+
+interface DriverParams {
+  id: string
+}
+server.get<{Params: DriverParams}>("/drivers/:id", async (request, response) => {
+  const id = parseInt(request.params.id)
+  const driver = drivers.find(item => item.id == id)
+
+  if (!driver) {
+    response.type("application/json").code(404)
+    return { message: "Driver Not Found" }
+  }
+
+  response.type("application/json").code(200)
+  return { driver }
+})
+
+server.listen({ port: 3000 }, () => {
+  console.log("Server is running...")
+})
+
