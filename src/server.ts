2c20bbd040c81828c55d3c0d0a78dd1157408734
André Nascimento <andre.neospace@gmail.com>
2026-06-11 13:51:30 -0300

    feat: drivers routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify"
 import cors from "@fastify/cors"
+import Drivers from "./controllers/drivers"
 
 const server = fastify({ logger: true })
 
@@ -7,47 +8,13 @@ server.register(cors, {
   origin: "*"
 })
 
-const teams = [
-  { id: 1, name: "Ferrari", base: "" },
-  { id: 2, name: "McLaren", base: "Woking, United Kingdom" },
-  { id: 3, name: "Mercedes", base: "Brackley, United Kingdom" },
-  { id: 4, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
-]
+server.get("/drivers", Drivers.getAllDrivers)
+server.get("/drivers/:id", Drivers.getDriverById)
+server.post("/drivers", Drivers.createDriver)
+server.put("/drivers/:id", Drivers.updateDriver)
+server.delete("/drivers/:id", Drivers.deleteDriver)
 
-const drivers = [
-  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
-  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
-  { id: 3, name: "Nando Morris", team: "McLaren" },
-  //{ id: , name: "", team: "" },
-]
-
-server.get("/teams", async (request, response) => {
-  response.type("application/json").code(200)
-  return { teams }
-})
-
-server.get("/drivers", async (request, response) => {
-  response.type("application/json").code(200)
-  return drivers
-})
-
-interface DriverParams {
-  id: string
-}
-server.get<{Params: DriverParams}>("/drivers/:id", async (request, response) => {
-  const id = parseInt(request.params.id)
-  const driver = drivers.find(item => item.id == id)
-
-  if (!driver) {
-    response.type("application/json").code(404)
-    return { message: "Driver Not Found" }
-  }
-
-  response.type("application/json").code(200)
-  return { driver }
-})
-
-server.listen({ port: 3000 }, () => {
+server.listen({ port: parseInt(process.env.PORT + "") || 3000 }, () => {
   console.log("Server is running...")
 })
 
